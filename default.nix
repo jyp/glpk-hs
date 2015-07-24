1721b58a7d1fe9b41559d2da1098074422c10702
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-07-24 13:30:50 +0200

    nix stuff

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, array, base, containers, deepseq, glpk, linear, mtl
+, stdenv
+}:
+mkDerivation {
+  pname = "glpk-hs";
+  version = "0.3.4";
+  src = ./.;
+  buildDepends = [ array base containers deepseq linear mtl ];
+  extraLibraries = [ glpk ];
+  description = "Comprehensive GLPK linear programming bindings";
+  license = stdenv.lib.licenses.bsd3;
+}
