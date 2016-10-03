1c28d09658e014ceb5f84def2adf2597b1d3e59a
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-10-03 09:33:52 +0200

    update example

--- examples/example1.hs
+++ examples/example1.hs
@@ -1,21 +1,29 @@
+import Prelude hiding (Num(..))
 
-import Data.LinearProgram.LPMonad
+import Algebra.Classes
+import Control.Monad.LPMonad
+import Data.LinearProgram.Common
 import Data.LinearProgram
 import Data.LinearProgram.GLPK
+import qualified Data.Map as M
+import Data.LinearProgram.LinExpr
 
 objFun :: LinFunc String Int
 objFun = linCombination [(10, "x1"), (6, "x2"), (4, "x3")]
 
+n *& v = linCombination [(n,v)]
+
 lp :: LP String Int
-lp = execLPM $ do	setDirection Max
-			setObjective objFun
-			leqTo (varSum ["x1", "x2", "x3"]) 100
-			leqTo (10 *^ var "x1" ^+^ 4 *& "x2" ^+^ 5 *^ var "x3") 600
-			leqTo (linCombination [(2, "x1"), (2, "x2"), (6, "x3")]) 300
-			varGeq "x1" 0
-			varBds "x2" 0 50
-			varGeq "x3" 0
-			setVarKind "x1" IntVar
-			setVarKind "x2" ContVar
+lp = execLPM $ do
+  setDirection Max
+  setObjective objFun
+  leqTo (add $ map var ["x1", "x2", "x3"]) 100
+  leqTo (10 *& "x1" + 4 *& "x2" + 5 *& "x3") 600
+  leqTo (linCombination [(2, "x1"), (2, "x2"), (6, "x3")]) 300
+  varGeq "x1" 0
+  varBds "x2" 0 50
+  varGeq "x3" 0
+  setVarKind "x1" IntVar
+  setVarKind "x2" ContVar
 
 main = print =<< glpSolveVars mipDefaults lp
